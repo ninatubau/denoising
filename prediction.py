35ddc1aad73af647e13cdf26ef0f45d56a851501
Nina Tubau Ribera <ninatubau9@hotmail.com>
2019-08-05 17:42:59 +1000

    Add number of tiles as argument

--- prediction.py
+++ prediction.py
@@ -32,6 +32,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Prediction arguments')
 parser.add_argument('path_data', type=str,help='Path to your input data: read the to_predict image')
 parser.add_argument('name_model', type=str,default ='my_model',help='Name of the model to use')
+parser.add_argument('--n_tiles', nargs="+",type=int, default=(1,4,4),help='Tuple of the number of tiles for every image axis to avoid out of memory problems when the input image is too large. Examples: 1 4 4')
 parser.add_argument('--axes', type=str,default='ZYX',help='Axes to indicate the semantic order of the images axes. Examples : ZYX, CXY ... ')
 parser.add_argument('--plot_prediction', type=bool,default =False,help='Plotting images of the prediction : True or False')
 
@@ -40,6 +41,7 @@ def main():
 	args = parser.parse_args()
 	path_data = args.path_data
 	name_model = args.name_model
+	n_tiles=tuple(args.n_tiles)
 	axes = args.axes
 	plot_prediction = args.plot_prediction
 	# # CARE model - our data
@@ -58,10 +60,11 @@ def main():
 		start =time.time()
 		x = imread(path_data+'to_predict/'+file_)
         #x = np.expand_dims(x,axis=3)
-		mid = int(x.shape[0]/2)
-		restored_1 = model.predict(x[0:mid], axes)
-		restored_2 = model.predict(x[mid:],axes)
-		restored = np.concatenate((restored_1,restored_2),axis=0)
+		#mid = int(x.shape[0]/2)
+		#restored_1 = model.predict(x[0:mid], axes)
+		#restored_2 = model.predict(x[mid:],axes)
+		#restored = np.concatenate((restored_1,restored_2),axis=0)
+		restored=model.predict(x,axes,n_tiles=n_tiles)
 		end = time.time()
 		print('Prediction time %s sec ' %(end - start))
 		print('Saving file: ',file_)
